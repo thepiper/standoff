cd46c4ceaf7802003ec25ae43eeaf93bed029aea
James Liu <liujames.q@gmail.com>
2016-01-19 16:59:35 -0500

    yo dawg I heard you like templates.

--- standoffish.py
+++ standoffish.py
@@ -1,27 +1,32 @@
 from flask import Flask
 from flask import *
 from jinja2 import Environment, PackageLoader, FileSystemLoader
-import flask
 import datetime
 
 app = Flask(__name__)
 
 env = Environment(loader=PackageLoader('standoffish'))
 
+baseTemplate = env.get_template('base.html')
+
 
 @app.route('/')
 def root():
     return redirect(url_for('home'))
 
-
 @app.route('/0')
 def home():
-    homeTemplate = env.get_template("home.html")
-    return render_template ( 
-            homeTemplate, 
-            year = datetime.date.today().year, 
-            css = url_for('static', filename='sobase.css'),
-            title = 0 )
+    post = render_template('content/2016-01-18.html')
+    year = datetime.date.today().year
+    css = url_for('static', filename='sobase.css')
+    title = 0 
+    
+    return render_template ([baseTemplate, year, css, title, post])
+
+@app.route('/photography')
+def photography():
+    galleryTemplate = env.get_template('gallery.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
